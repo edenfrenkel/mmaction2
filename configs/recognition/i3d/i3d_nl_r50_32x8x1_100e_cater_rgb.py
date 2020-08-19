029d2400f14a3138d6cdee2949e9a05cd0e6a80c
edenfrenkel <eden060398@gmail.com>
2020-08-19 15:06:22 +0300

    Update i3d_nl_r50_32x8x1_100e_cater_rgb.py

--- configs/recognition/i3d/i3d_nl_r50_32x8x1_100e_cater_rgb.py
+++ configs/recognition/i3d/i3d_nl_r50_32x8x1_100e_cater_rgb.py
@@ -89,12 +89,15 @@ data = dict(
         data_prefix=data_root_val,
         pipeline=test_pipeline))
 # optimizer
-optimizer = dict(
-    type='SGD', lr=0.00125, momentum=0.9,
-    weight_decay=0.0001)
-optimizer_config = dict(grad_clip=dict(max_norm=40, norm_type=2))
+optimizer = dict(type='Adam')
+optimizer_config = dict()
+lr_config = dict(policy='fixed')
+# optimizer = dict(
+#     type='SGD', lr=0.00125, momentum=0.9,
+#     weight_decay=0.0001)
+# optimizer_config = dict(grad_clip=dict(max_norm=40, norm_type=2))
 # learning policy
-lr_config = dict(policy='step', step=[40, 80])
+# lr_config = dict(policy='step', step=[40, 80])
 total_epochs = 100
 checkpoint_config = dict(interval=5)
 evaluation = dict(
