031e39dc0d4df58d283c9582bd23987c2a5649f7
edenfrenkel <eden060398@gmail.com>
2020-08-23 23:08:36 +0300

    Update i3d_nl_r50_32x2x5_50e_cater_rgb.py

--- configs/recognition/i3d/i3d_nl_r50_32x2x5_50e_cater_rgb.py
+++ configs/recognition/i3d/i3d_nl_r50_32x2x5_50e_cater_rgb.py
@@ -98,7 +98,7 @@ data = dict(
         pipeline=test_pipeline))
 # optimizer
 optimizer = dict(
-    type='SGD', lr=0.0003125, momentum=0.9,
+    type='SGD', lr=0.000625, momentum=0.9,
     weight_decay=0.0001)  # this lr is used for 4 gpus
 optimizer_config = dict(grad_clip=dict(max_norm=40, norm_type=2))
 # learning policy
