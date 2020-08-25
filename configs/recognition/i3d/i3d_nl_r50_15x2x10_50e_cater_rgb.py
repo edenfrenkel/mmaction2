80b6600ad9d164a41d07386061a4d7c8bb46acf1
edenfrenkel <eden060398@gmail.com>
2020-08-25 22:12:41 +0300

    Update i3d_nl_r50_15x2x10_50e_cater_rgb.py

--- configs/recognition/i3d/i3d_nl_r50_15x2x10_50e_cater_rgb.py
+++ configs/recognition/i3d/i3d_nl_r50_15x2x10_50e_cater_rgb.py
@@ -41,7 +41,7 @@ img_norm_cfg = dict(
     mean=[123.675, 116.28, 103.53], std=[58.395, 57.12, 57.375], to_bgr=False)
 train_pipeline = [
     dict(type='DecordInit'),
-    dict(type='SequentialSampleFrames', clip_len=15, frame_interval=2, num_clips=10),
+    dict(type='SequentialSampleFrames', clip_len=15, frame_interval=2, num_clips=10, jitter=True),
     dict(type='DecordDecode'),
     dict(type='Resize', scale=0.8, keep_ratio=True),
     dict(type='Normalize', **img_norm_cfg),
