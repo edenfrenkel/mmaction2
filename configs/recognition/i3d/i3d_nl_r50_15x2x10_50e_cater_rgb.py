5e55079bb9493280905d87400e6077c225ec1de5
edenfrenkel <eden060398@gmail.com>
2020-08-27 23:16:33 +0300

    Update i3d_nl_r50_15x2x10_50e_cater_rgb.py

--- configs/recognition/i3d/i3d_nl_r50_15x2x10_50e_cater_rgb.py
+++ configs/recognition/i3d/i3d_nl_r50_15x2x10_50e_cater_rgb.py
@@ -105,7 +105,7 @@ optimizer = dict(
 optimizer_config = dict(grad_clip=dict(max_norm=40, norm_type=2))
 # learning policy
 lr_config = dict(policy='fixed')
-total_epochs = 20
+total_epochs = 50
 checkpoint_config = dict(interval=5)
 evaluation = dict(
     interval=1, metrics=['mean_average_precision'],
