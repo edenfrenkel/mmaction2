6dfb7373380f4d90e165d14a2b53920343841c8d
edenfrenkel <eden060398@gmail.com>
2020-08-27 22:46:20 +0300

    Update recognizer3d.py

--- mmaction/models/recognizers/recognizer3d.py
+++ mmaction/models/recognizers/recognizer3d.py
@@ -14,7 +14,7 @@ class Recognizer3D(BaseRecognizer):
 
         x = self.extract_feat(imgs)
         cls_score = self.cls_head(x, num_segs)
-        gt_labels = labels.squeeze()
+        gt_labels = labels.view(cls_score)
         loss = self.cls_head.loss(cls_score, gt_labels)
 
         return loss
