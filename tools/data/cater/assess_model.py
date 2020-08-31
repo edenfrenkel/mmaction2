b7f36f9a0ccf80ea7e527d71632cb6d004a0c0d6
edenfrenkel <eden060398@gmail.com>
2020-08-31 21:46:53 +0300

    Update assess_model.py

--- tools/data/cater/assess_model.py
+++ tools/data/cater/assess_model.py
@@ -25,9 +25,12 @@ def assess_by_class(video_infos, results, classes):
         true_negatives += (1 - labels) * (1 - result)
         confusions[just_labels == 1] += just_result
 
-    conf_prop = confusions.max(axis=1) / confusions.sum(axis=1)
+    conf_max = confusions.max(axis=1)
+    conf_sum = confusions.sum(axis=1)
+    conf_prop = np.divide(conf_max, conf_sum, out=np.zeros_like(conf_max), where=conf_max!=0)
     conf_ind = confusions.argmax(axis=1)
-    conf_dense = confusions / confusions.sum(axis=1)[:, np.newaxis]
+    conf_sum = conf_sum[:, np.newaxis]
+    conf_dense = np.divide(confusions, conf_sum, out=np.zeros_like(confusions), where=conf_sum!=0)
 
     greater_key = lambda x: -x[1]
 
@@ -50,48 +53,35 @@ def assess_by_class(video_infos, results, classes):
 
 
 def assess_by_video(video_infos, results, classes):
-    true_positives = np.zeros(len(video_infos), dtype=np.float)
-    false_positives = np.zeros(len(video_infos), dtype=np.float)
-    true_negatives = np.zeros(len(video_infos), dtype=np.float)
-    false_negatives = np.zeros(len(video_infos), dtype=np.float)
-    classifications = {}
-    predictions = {}
+    just_labels = np.zeros((len(video_infos), len(classes)), dtype=np.float)
+    just_results = np.zeros((len(video_infos), len(classes)), dtype=np.float)
+    true_pos = np.zeros((len(video_infos), len(classes)), dtype=np.float)
     names = [re.search(r'CATER_new_(\d+)', info['filename']).group(0) for info in video_infos]
 
     for i in range(len(video_infos)):
-        labels = video_infos[i]['label']
-        cls = []
-        preds = []
-        for j in range(len(classes)):
-            if labels[j] == 1:
-                cls.append(classes[j])
-                if results[i][j] > 0:
-                    preds.append(classes[j])
-                    true_positives[i] += 1
-                else:
-                    false_negatives[i] += 1
-            else:
-                if results[i][j] > 0:
-                    preds.append(classes[j])
-                    false_positives[i] += 1
-                else:
-                    true_negatives[i] += 1
-        classifications[names[i]] = cls
-        predictions[names[i]] = preds
+        label = video_infos[i]['label'].cpu().numpy()
+        result = np.array([1 if results[i][j] > 0 else 0
+                           for j in range(len(classes))], dtype=np.float)
+        both = label * result
+        just_label = label - both
+        just_result = result - both
 
-    greater_key = lambda x: -x[1]
+        just_labels[i] += just_label
+        just_results[i] += just_result
+        true_pos[i] += both
 
-    tp_list = sorted([(names[i], true_positives[i]) for i in range(len(names))], key=greater_key)
-    tn_list = sorted([(names[i], true_negatives[i]) for i in range(len(names))], key=greater_key)
-    fp_list = sorted([(names[i], false_positives[i]) for i in range(len(names))], key=greater_key)
-    fn_list = sorted([(names[i], false_negatives[i]) for i in range(len(names))], key=greater_key)
+    len_key = lambda x: -len(x[1])
 
-    return {'true_positives': tp_list,
-            'false_positives': fp_list,
-            'true_negatives': tn_list,
-            'false_negatives': fn_list,
-            'classifications': classifications,
-            'predictions': predictions}
+    jl_list = sorted([[names[i], [classes[j] for j in range(len(classes)) if just_labels[i, j] == 1]]
+                      for i in range(len(names))], key=len_key)
+    jr_list = sorted([[names[i], [classes[j] for j in range(len(classes)) if just_results[i, j] == 1]]
+                      for i in range(len(names))], key=len_key)
+    both_list = sorted([[names[i], [classes[j] for j in range(len(classes)) if true_pos[i, j] == 1]]
+                        for i in range(len(names))], key=len_key)
+
+    return {'just_label': jl_list,
+            'just_result': jr_list,
+            'both': both_list}
 
 
 def assess_results(video_infos, results, filename, is_comp=False):
