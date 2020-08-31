9c94446c3eb8e01967bde649a8a7a44752ceec7d
edenfrenkel <eden060398@gmail.com>
2020-08-31 20:13:55 +0300

    Update assess_model.py

--- tools/data/cater/assess_model.py
+++ tools/data/cater/assess_model.py
@@ -9,20 +9,24 @@ def assess_by_class(video_infos, results, classes):
     false_positives = np.zeros(len(classes), dtype=np.float)
     true_negatives = np.zeros(len(classes), dtype=np.float)
     false_negatives = np.zeros(len(classes), dtype=np.float)
+    confusions = np.zero((len(classes), len(classes)), dtype=np.float)
 
     for i in range(len(video_infos)):
         labels = video_infos[i]['label']
-        for j in range(len(classes)):
-            if labels[j] == 1:
-                if results[i][j] > 0:
-                    true_positives[j] += 1
-                else:
-                    false_negatives[j] += 1
-            else:
-                if results[i][j] > 0:
-                    false_positives[j] += 1
-                else:
-                    true_negatives[j] += 1
+        result = np.array([1 if results[i][j] > 0 else 0
+                           for j in range(len(classes))], dtype=np.float)
+        both = labels * result
+        just_labels = labels - both
+        just_result = result - both
+
+        true_positives += both
+        false_negatives += just_labels
+        false_positives += just_result
+        true_negatives += (1 - labels) * (1 - result)
+        confusions[just_labels == 1] += just_result
+
+    conf_prop = confusions.max(axis=1) / confusions.sum(axis=1)
+    conf_ind = confusions.argmax(axis=1)
 
     greater_key = lambda x: -x[1]
 
@@ -30,11 +34,13 @@ def assess_by_class(video_infos, results, classes):
     tn_list = sorted([(classes[i], true_negatives[i]) for i in range(len(classes))], key=greater_key)
     fp_list = sorted([(classes[i], false_positives[i]) for i in range(len(classes))], key=greater_key)
     fn_list = sorted([(classes[i], false_negatives[i]) for i in range(len(classes))], key=greater_key)
+    conf_list = sorted([(classes[i], conf_prop[i], classes[conf_ind[i]]) for i in range(len(classes))], key=greater_key)
 
     return {'true_positives': tp_list,
             'false_positives': fp_list,
             'true_negatives': tn_list,
-            'false_negatives': fn_list}
+            'false_negatives': fn_list,
+            'confusion': conf_list}
 
 
 def assess_by_video(video_infos, results, classes):
